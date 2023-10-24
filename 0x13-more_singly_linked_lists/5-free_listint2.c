ef4319f95445d13aee9bb2b849d06f6fc4bcc09a
henryiyke0x <spacecannon7@gmail.com>
2023-10-24 14:34:09 -0700

    task 7

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,15 +1,23 @@
+#include "lists.h"
 
- listint_t *temp;
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
 
- if (head == NULL)
- return;
-
- while (*head)
- {
- temp = (*head)->next;
- free(*head);
- *head = temp;
- }
-
- *head = NULL;
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,24 +1,29 @@
 #include "lists.h"
 
 /**
- * pop_listint - deletes the head node of a linked list
- * @head: pointer to the first element in the linked list
- *
- * Return: the data inside the elements that was deleted,
- * or 0 if the list is empty
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ * Return: head node's data.
  */
 int pop_listint(listint_t **head)
 {
- listint_t *temp;
- int num;
+	int hnode;
+	listint_t *h;
+	listint_t *currnt;
 
- if (!head || !*head)
- return (0);
+	if (*head == NULL)
+	return (0);
 
- num = (*head)->n;
- temp = (*head)->next;
- free(*head);
- *head = temp;
+	currnt = *head;
 
- return (num);
+	hnode = currnt->n;
+
+	h = currnt->next;
+
+	free(currnt);
+
+	*head = h;
+
+	return (hnode);
 }
